2013d5f9d07a954589bd7fb7a70ad0e0d9e71a68
Alexander Redder <109695232+KanzanKohi@users.noreply.github.com>
2024-03-26 10:46:15 +0100

    .

--- src/main/java/org/g5/Main.java
+++ src/main/java/org/g5/Main.java
@@ -1,34 +1,34 @@
 package org.g5;
 
+import org.antlr.v4.runtime.ANTLRErrorListener;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.Parser;
+import org.antlr.v4.runtime.RecognitionException;
+import org.antlr.v4.runtime.Recognizer;
+import org.antlr.v4.runtime.atn.ATNConfigSet;
+import org.antlr.v4.runtime.dfa.DFA;
 import org.antlr.v4.runtime.tree.ParseTree;
-import org.g5.parser.CustomVisitor;
 import org.g5.parser.GrammarLexer;
 import org.g5.parser.GrammarParser;
 
 import java.io.IOException;
+import java.util.BitSet;
 
 public class Main {
     public static void main(String[] args) {
-        readFile("test.nn");
-    }
-
-    public static void readFile(String path) {
         try {
-            CharStream charStream = CharStreams.fromFileName(path);
+            CharStream charStream = CharStreams.fromFileName("testfiles/test_main");
             GrammarLexer lexer = new GrammarLexer(charStream);
             GrammarParser parser = new GrammarParser(new CommonTokenStream(lexer));
 
-            parser.addParseListener(new ParserListener());
-            ParseTree parseTree = parser.program();
-            System.out.println(parseTree.toStringTree());
-            System.out.println(parseTree.toStringTree(parser));
-            CustomVisitor customVisitor = new CustomVisitor();
-            customVisitor.visit(parseTree);
+            ParseTree topTree = parser.program();
 
-        } catch (IOException e) {
+            Visitor visitor = new Visitor();
+            visitor.visit(topTree);
+        }
+        catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
