054d670c05a1eea82086551b866fb8ab103ca093
Alexander Redder <109695232+KanzanKohi@users.noreply.github.com>
2024-03-26 11:38:27 +0100

    .daw

--- src/main/java/org/g5/ValueTable.java
+++ src/main/java/org/g5/ValueTable.java
@@ -3,14 +3,5 @@ package org.g5;
 import java.util.HashMap;
 
 public class ValueTable extends HashMap<String, ValueTableEntry> {
-    public Object getValue(String id) {
-        return this.get(id);
-    }
-    public Object insertValue(Class<?> type, String id) {
-        return this.get()
-    }
 
-    public static ValueTableEntry {
-
-    }
 }
--- src/main/java/org/g5/Visitor.java
+++ src/main/java/org/g5/Visitor.java
@@ -1,14 +1,17 @@
 package org.g5;
 
 import org.g5.parser.GrammarBaseVisitor;
+import org.g5.parser.GrammarParser;
 import org.g5.parser.GrammarParser.ExpressionContext;
 import org.g5.parser.GrammarParser.ExpressionStringContext;
 import org.g5.parser.GrammarParser.StringContext;
 
+import java.awt.*;
+
 public class Visitor extends GrammarBaseVisitor<Object> {
     @Override
     public String visitExpressionString(ExpressionStringContext ctx) {
-        System.out.println(visitChildren(ctx));
+        //System.out.println(visitChildren(ctx));
         return null;
     }
 
@@ -17,5 +20,25 @@ public class Visitor extends GrammarBaseVisitor<Object> {
         txt = txt.substring(1, txt.length() - 1);
 
         return txt;
+    }
+
+    @Override
+    public Integer visitInt(GrammarParser.IntContext ctx) {
+        return Integer.parseInt(ctx.getText());
+    }
+
+    @Override
+    public Float visitFloat(GrammarParser.FloatContext ctx) {
+        return Float.parseFloat(ctx.getText());
+    }
+
+    @Override
+    public String visitLinearLayer(GrammarParser.LinearLayerContext ctx) {
+        return ctx.getText();
+    }
+
+    @Override
+    public String visitSequentialContainer(GrammarParser.SequentialContainerContext ctx) {
+        return ctx.getText();
     }
 }
